4ac3e9f26dd584cadd98c6a06325de8ca586cc4e
ChenLin Wang <wangchenlin2001@icloud.com>
2026-06-24 21:43:45 +0800

    V1.0.5.1 更新依赖

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library( name: "FileStorageDriver", targets: ["FileStorageDriver"] )
     ],
     dependencies: [
-        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-server.git", from: "1.2.8"),
+        .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-server.git", from: "1.2.9"),
         .package(url: "https://github.com/whooshing-workshop/whooshing.toolbox-file-storage", from: "1.0.9"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.9.1")
     ],
---
+++ Sources/FileStorageDriver/Exports.swift
@@ -0,0 +1,2 @@
+@_exported import WhooshingServer
+@_exported import FileStorage
